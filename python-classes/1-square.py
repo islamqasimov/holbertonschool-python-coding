71b1c56cd4204d847c378de0c4dede5622916d3d
islamqasimov <12495@holbertonstudents.com>
2025-11-23 17:29:53 +0000

    autocommit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -13,8 +13,7 @@ class Square:
         """
         if isinstance(size, int):
             if size < 0:
-                raise ValueError("size must be >= 0") 
+                raise ValueError("size must be >= 0")
             self.__size = size
         else:
             raise TypeError("size must be an integer")
-        
