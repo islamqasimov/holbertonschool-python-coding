5bdff671050fceec41ec6a6e98be2468d2cdc3c7
islamqasimov <12495@holbertonstudents.com>
2025-11-23 17:28:06 +0000

    autocommit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Square module
+"""
+
+
+class Square:
+    """Represent a square with a private size attribute."""
+    def __init__(self, size=0):
+        """Initialize a new Square instance with a private size attribute.
+        The size is stored privately as an implementation detail and is not
+        validated at this stage.
+        """
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0") 
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        
